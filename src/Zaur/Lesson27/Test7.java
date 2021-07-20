4f04dc62384c4578eb53d6ef379639d294ea0089
Misha Mosi <mihmosi@gmail.com>
2021-07-20 15:40:24 +0300

    init

---
+++ src/Zaur/Lesson27/Test7.java
@@ -0,0 +1,35 @@
+package Zaur.Lesson27;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Test7 {
+    public static void main(String[] args) {
+        StringBuilder result = new StringBuilder("");
+        try {
+            File f = new File("test9.txt");
+            System.out.println("Obyect sozdalsya");
+            FileInputStream fis = new FileInputStream(f);
+            System.out.println("Strim sozdalsya");
+            int counter = 0;
+            while (true) {
+                counter++;
+                result.append(fis.read());
+                System.out.println("Informaciya chitaetsya");
+                if (counter == 3){
+                    fis.close();
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Exception 1");
+        } catch (IOException a) {
+            System.out.println("Exception 2");
+        } finally {
+            System.out.println("Eto finally");
+            System.out.println(result);
+        }
+    }
+}
